1b035a9ffe43968cbc1be37200c96b76c65795f4
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-18 15:31:16 -0300

    Adjusting license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.majorcluster/clj-data-adapter "0.8.1"
+(defproject org.clojars.majorcluster/clj-data-adapter "0.8.2"
   :description "A Clojure data adapter library to convert your data between your layers"
   :url "https://github.com/mtsbarbosa/clj-data-adapter"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :source-paths ["src/clj"]
   :profiles {:dev {:plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.3.17"]]
