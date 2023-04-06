e349ca1fa93b29a577169800a5b0b42cf0786f4d
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-04-06 19:43:49 -0300

    Setup deploy script

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.majorcluster/clj-data-adapter "0.8.3"
+(defproject org.clojars.majorcluster/clj-data-adapter "0.8.4"
   :description "A Clojure data adapter library to convert your data between your layers"
   :url "https://github.com/majorcluster/clj-data-adapter"
   :license {:name "The MIT License"
