ea6acd09d216790707ce3017568e3024d4056c46
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-04-06 22:52:50 -0300

    Setup deploy script

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject org.clojars.majorcluster/clj-data-adapter "0.8.5"
+(defproject org.clojars.majorcluster/clj-data-adapter "0.8.6"
   :description "A Clojure data adapter library to convert your data between your layers"
   :url "https://github.com/majorcluster/clj-data-adapter"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :source-paths ["src/clj"]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :profiles {:dev {:plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.3.17"]]
                    :dependencies [[nubank/matcher-combinators "3.8.3"]]}}
   :aliases {"diagnostics"     ["clojure-lsp" "diagnostics"]
