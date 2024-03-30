e0267cc0b680902075caf98718cd2ad83246b275
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2024-03-30 12:24:57 -0300

    bumping libraries

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject org.clojars.majorcluster/clj-data-adapter "0.8.6"
+(defproject org.clojars.majorcluster/clj-data-adapter "0.9.0"
   :description "A Clojure data adapter library to convert your data between your layers"
   :url "https://github.com/majorcluster/clj-data-adapter"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.2"]]
   :source-paths ["src/clj"]
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/clojars_username
                                     :password :env/clojars_password}]]
   :profiles {:dev {:plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.3.17"]]
-                   :dependencies [[nubank/matcher-combinators "3.8.3"]]}}
+                   :dependencies [[nubank/matcher-combinators "3.9.1"]]}}
   :aliases {"diagnostics"     ["clojure-lsp" "diagnostics"]
             "format"          ["clojure-lsp" "format" "--dry"]
             "format-fix"      ["clojure-lsp" "format"]
